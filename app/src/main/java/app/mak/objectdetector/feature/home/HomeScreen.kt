396bc21f33365900605a5e65e685b613db45a41b
Ashish Mohite <ashishmohite1996@gmail.com>
2026-05-06 17:00:30 +0530

    Create Home UI

---
+++ app/src/main/java/app/mak/objectdetector/feature/home/HomeScreen.kt
@@ -0,0 +1,36 @@
+package app.mak.objectdetector.feature.home
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import app.mak.objectdetector.R
+import app.mak.objectdetector.ui.theme.ObjectDetectorTheme
+
+@Composable
+internal fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(onClick = {}) {
+            Text(stringResource(R.string.capture_image))
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun HomeScreenPreview() {
+    ObjectDetectorTheme {
+        HomeScreen()
+    }
+}
